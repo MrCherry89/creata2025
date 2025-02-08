eabb8fbbf213619d543114eae87e37b75f2b42dc
MrCherry89 <abgarnati@yandex.ru>
2025-02-09 02:15:40 +0400

    ok

--- js/script.js
+++ js/script.js
@@ -1,30 +1,5 @@
 $(document).ready(function () {
-  gsap.registerPlugin(ScrollTrigger);
 
-  // Анимация для букв как кубиков
-  gsap.fromTo(
-    ".animation-text span",
-    {
-      scale: 0, // Исходный размер (как точка)
-      rotateX: 90, // Поворот по X
-      opacity: 0, // Полностью невидимо
-    },
-    {
-      scale: 1, // Конечный размер
-      rotateX: 0, // Прямое положение
-      opacity: 1, // Полностью видно
-      duration: 0.8, // Длительность анимации каждой буквы
-      ease: "back.out(1.7)", // Эффект "вылета" кубика
-      stagger: 0.2, // Задержка между анимацией каждой буквы
-      scrollTrigger: {
-        trigger: ".animation-text", // Триггер анимации
-        start: "top 80%", // Когда начнется анимация
-        end: "top 50%", // Когда анимация завершится
-        scrub: false, // Не привязывается к скроллу
-        once: true, // Анимация выполняется один раз
-      },
-    }
-  );
 
   $(".phone-number-input").inputmask({
     mask: "+7 (999)-999-999-9",
